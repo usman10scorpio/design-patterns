4f7a90d349c6114402eba1da9894696d3232cb30
usman10scorpio <usman10scorpio@gmail.com>
2022-12-08 11:27:00 +0500

    main :: pattern class implemented

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('here')
+
+// class design pattern
+class Car {
+    constructor(doors,engine,color) {
+        this.doors = doors;
+        this.engine = engine;
+        this.color = color;
+    }
+}
+
+class Suv extends Car {
+    constructor(doors,engine,color) {
+        super(doors,engine,color);
+        this.wheels = 4;
+    }
+
+
+}
+
+const civic = new Car(4,'vvti','red')
+const fortrunner = new Suv(4,'vvti','red')
+
+console.log(civic)
+console.log(fortrunner)
